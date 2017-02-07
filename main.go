6c20fcc26c18274b607e1198dcfac6a383ccc1cb
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-02-06 22:46:06 -0200

    First steps actually worked :)

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
 import "fmt"
+import telegram "github.com/go-telegram-bot-api/telegram-bot-api"
 
+// Code from https://github.com/go-telegram-bot-api/telegram-bot-api and
+// adapted to run on this application
 func main() {
     fmt.Printf("Let's do this!\n")
+
+    // Registering bot
+    var token string
+    fmt.Scanln(&token)
+    bot, err := telegram.NewBotAPI(token)
+    if err != nil {
+        fmt.Println(err)
+        panic(err)
+    }
+    bot.Debug = true
+    fmt.Printf("Authorized on account %s", bot.Self.UserName)
+
+    // Getting updates
+    u := telegram.NewUpdate(0)
+    u.Timeout = 60
+    updates, err := bot.GetUpdatesChan(u)
+    for update := range updates {
+        if update.Message == nil {
+            continue
+        }
+
+        fmt.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+        msg := telegram.NewMessage(update.Message.Chat.ID, update.Message.Text)
+        msg.ReplyToMessageID = update.Message.MessageID
+
+        bot.Send(msg)
+    }
 }
