160483c8868fa58fab1b1bbb91a3a8ef1a586621
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-05-15 17:38:44 -0300

    Refactoring code

--- main.go
+++ main.go
@@ -2,36 +2,33 @@ package main
 
 import (
     "os"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
     "github.com/ishiikurisu/logeybot/controller"
 )
 
 func main() {
     // Setup bot
-	bot, err := tgbotapi.NewBotAPI(os.Args[1])
+	bot, err := telegram.NewBotAPI(os.Args[1])
 	if err != nil {
-		log.Panic(err)
+        panic(err)
 	}
 	bot.Debug = true
-	u := tgbotapi.NewUpdate(0)
+	u := telegram.NewUpdate(0)
 	u.Timeout = 60
 	updates, err := bot.GetUpdatesChan(u)
-    // Setup memory
+    // Load previous states from memory
     controllers := make(map[int64]*controller.Controller)
     // Putting bot to work depending on messages
 	for update := range updates {
-		if update.Message == nil {
-			continue
+		if update.Message != nil {
+            // TODO Do this on parallel
+            identification := update.Message.Chat.ID
+            if _, ok := controllers[identification]; !ok {
+                dummy := controller.NewController(identification)
+                controllers[identification] = &dummy
+            }
+    		msg := telegram.NewMessage(identification, controllers[identification].Listen(update.Message.Text))
+    		bot.Send(msg)
 		}
-
-        identification := update.Message.Chat.ID
-        _, ok := controllers[identification]
-        if !ok {
-            e := controller.NewController(identification)
-            controllers[identification] = &e
-        }
-        c := controllers[identification]
-		msg := tgbotapi.NewMessage(identification, c.Listen(update.Message.Text))
-		bot.Send(msg)
 	}
 }
