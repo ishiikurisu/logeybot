23d19900193f7b4cf2cbe774147ae74d86c4d2d2
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-05-21 21:48:44 -0300

    Creating a logey for each user

--- controller/controller.go
+++ controller/controller.go
@@ -5,6 +5,7 @@ import (
     "github.com/ishiikurisu/logeybot/model"
     "github.com/ishiikurisu/logeybot/view"
     "strings"
+    "strconv"
 )
 
 type Controller struct {
@@ -28,25 +29,41 @@ func NewController(inlet int64) Controller {
 
 // Generates the correct answer depending on the current controller's state
 // the command given by the user.
-func (c *Controller) Listen(message string) string {
+func (controller *Controller) Listen(message string) string {
     outlet := ""
 
-    if c.View.IsUp() {
-        c.View.Listen(message)
-        if !c.View.IsUp() {
+    if controller.View.IsUp() {
+        controller.View.Listen(message)
+        if !controller.View.IsUp() {
             // TODO Save data on memory
+            controller.Dump()
             // QUESTION Will this ending depend on the conversation kind?
             outlet = "Data saved on memory!"
-            c.View = view.CreateEmptyConversation()
+            controller.View = view.CreateEmptyConversation()
         } else {
-            outlet = c.View.Speak()
+            outlet = controller.View.Speak()
         }
     } else if strings.HasPrefix(message, "/add") {
-        c.View = view.NewAdditionConversation()
-        outlet = c.View.Speak()
+        controller.View = view.NewAdditionConversation()
+        outlet = controller.View.Speak()
+    } else if strings.HasPrefix(message, "/get") {
+        // TODO Add retrieve log logic
+        outlet = controller.Logey.ToString()
     } else {
         outlet = "wtf?"
     }
 
     return outlet
+}
+
+// Creates a new entry and saves it to memory
+func (controller *Controller) Dump() {
+    answers := controller.View.Answers
+    what := answers[0]
+    howMuch, oops := strconv.ParseFloat(answers[1], 64)
+    if oops != nil {
+        panic(oops)
+    }
+    controller.Logey.Add(what, howMuch)
+    // TODO Save logey on memory
 }
